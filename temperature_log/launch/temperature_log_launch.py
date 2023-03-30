26be4f58b6f2296ea2d2b3f6969130e99a40d990
Marco Fincato <marcofincato777@gmail.com>
2023-03-30 17:18:03 +0200

    Added launch file

---
+++ temperature_log/launch/temperature_log_launch.py
@@ -0,0 +1,14 @@
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='temperature_log',
+            executable='publisher',
+            name='publisher'),
+        launch_ros.actions.Node(
+            package='temperature_log',
+            executable='logger',
+            name='logger'),
+  ])
